1385645277d27c5ed11a8ae93307a9b9c463c862
victor.arana <victor.arana@walmart.com>
2020-09-15 16:58:05 -0500

    :alembic: select-component, test select component
    Changes committed:
    	modified:   src/app/dashboard/select/select.component.html
    	modified:   src/app/dashboard/select/select.component.ts

--- src/app/dashboard/select/select.component.ts
+++ src/app/dashboard/select/select.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Food {
+    value: string;
+    viewValue: string;
+}
+  
+
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
@@ -7,6 +13,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectComponent implements OnInit {
 
+  foods: Food[] = [
+        {value: 'steak-0', viewValue: 'Steak'},
+        {value: 'pizza-1', viewValue: 'Pizza'},
+        {value: 'tacos-2', viewValue: 'Tacos'}
+      ];
   constructor() { }
 
   ngOnInit() {
